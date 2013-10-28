15c36e1e8209e77c7132dd14089c03e9e8aa9619
Simon Nickel <simonnickel@googlemail.com>
2013-10-28 11:55:41 +0100

    fix safari hover bug

--- simplery.js
+++ simplery.js
@@ -1,60 +1,78 @@
 /*
+ * SETTINGS
+*/
+var hover_zoom_1 = -200;
+var hover_zoom_2 = -100;
+var hover_zoom_3 = -14;
+var hover_zoom_4 = 10;
+var hover_zoom_5 = 20;
+var hover_offset_border = 10;
+
+
+/*
+ * TRIGGER 
+*/
+
+/*
  * when html is loaded
 */
 $(document).ready(function() {
-	var galeries = $(".simplery-nojs");
-	
-	galeries.simpleryInit();
+
+	$(".simplery-nojs").each(function() {
+		$(this).removeClass("simplery-nojs");
+		$(this).addClass("simplery");
+	});
+
 });
 
 /*
  * when css+images are loaded
 */
 $(window).load(function() {
+	
+	// layout
+	$(".simplery").each(function() {
+		$(this).simpleryBoxLayout();
+	});
+
+	// image hover
+	$(".simplery li").hover(
+		function() {$(this).simpleryImageHover(0);},
+		function() {$(this).simpleryImageHover(1);}
+	);
+	
+});
+
+/*
+ * when window is resized
+*/
+$(window).resize(function() {
 	var galeries = $(".simplery");
 	
 	// layout
 	galeries.each(function() {
-		$(this).simpleryManipulate();
+		$(this).simpleryBoxResize();
 	});
-
-	// hover
-	$(".simplery li").hover(
-		function() {$(this).simpleryHover(0);},
-		function() {$(this).simpleryHover(1);}
-	);
-
 });
 
-/*
- * when html is loaded
-*/
-(function( $ ){$.fn.simpleryInit = function() {
-	var galery = $(this);
+// fix iOS hover after back button, see: https://coderwall.com/p/bevnew
+if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+    $(window).on('pageshow', function(e) {
+        if (e.originalEvent.persisted) {
+            window.location.reload();
+        }
+    });
+}
 
-	galery.removeClass("simplery-nojs");
-	galery.addClass("simplery");
-
-	return this;
-};})( jQuery );
 
 /*
- * when css+images are loaded
+ * FUNCTIONS
 */
-(function( $ ){$.fn.simpleryManipulate = function() {
-	var galery = $(this);
-	var width = parseFloat(galery.css("width"));
-
-	galery.simpleryLayout();
-
-
-	return this;
-};})( jQuery );
 
 /*
- * layout
+ * layout of the simplery box
 */
-(function( $ ){$.fn.simpleryLayout = function() {
+(function( $ ){$.fn.simpleryBoxLayout = function() {
 	var galery = $(this);
 	var ul = galery.find("ul");
 	var li = galery.find("li");
@@ -65,73 +83,113 @@ $(window).load(function() {
 	ul.css("font-size", "0px");
 	li.css("font-size", fontSize);
 
-
 	// adjust size to get squares
-	var blockWidth = li.width();
-	li.height(blockWidth);
-
-	img.each(function() {
-		var img = $(this);
-		var ratio = img.width() / img.height();
-
-		// portrait: move img to top
-		if (img.width() < img.height()) {
-			img.width(blockWidth);
-			img.height(blockWidth * 1/ratio);
-			img.css("top", (img.height() - img.width()) / -2);
-		}
-		else { // landscape: make bigger and move left
-			img.height(blockWidth);
-			img.width(blockWidth * ratio);
-			img.css("left", (img.width() - img.height()) / -2);
-		}
-	});	
+	galery.simpleryBoxResize();
 
 	return this;
 };})( jQuery );
 
 /*
- * hover
+ * resize the galery box
 */
-(function( $ ){$.fn.simpleryHover = function(inout) {
-	var li = $(this);
-	var img = li.find("img");
-	var ratio = img.width() / img.height();
+(function( $ ){$.fn.simpleryBoxResize = function() {
+	var galery = $(this);
+	var li = galery.find("li");
+	var img = galery.find("img");
 
-	if (inout == 0) {
-		//alert(parseFloat(img.css("top"))+10);
-		img.css({
-			transition : 'width 0.3s, height 0.3s, left 0.3s, top 0.3s'
-		});
-		img.css("width", parseFloat(img.css("width"))+20*ratio);
-		img.css("height", parseFloat(img.css("height"))+20*ratio);
-		img.css("left", parseFloat(img.css("left"))-10);
-		img.css("top", parseFloat(img.css("top"))-10);
-		//alert(img.css("top"));
-		/*
-		li.css("overflow", "visible");
-		li.css("z-index", "9999");
+	var blockWidth = li.width();
+	
+	li.each(function() {
+		$(this).height(blockWidth);
+	});
 
-		img.css("position", "absolute");
-		*/
-	}
-	else {
-		img.css("width", parseFloat(img.css("width"))-20*ratio);
-		img.css("height", parseFloat(img.css("height"))-20*ratio);
-		img.css("left", parseFloat(img.css("left"))+10);
-		img.css("top", parseFloat(img.css("top"))+10);
-		/*
-		li.css("overflow", "hidden");
-		li.css("z-index", "auto");
+	// center and resize images
+	img.each(function() {
+		$(this).simpleryBoxResizeImage(blockWidth);
 
-		img.css("position", "relative");
-		*/
-	}
+	});	
 
-	//alert('test');
+
 
 	return this;
 };})( jQuery );
+
+
+/*
+ * hover
+*/
+(function( $ ){$.fn.simpleryImageHover = function(inout) {
+	var li = $(this);
+	var ul = li.parent();
+	var galery = ul.parent();
+	var img = li.find("img");
+
+	var blockWidth = li.width();
+	var ratio = Math.round(img.width() / img.height() * 10) / 10;
+
+	if (inout == 0) {
+		var hover_zoom = 0;
+		if (galery.hasClass("simplery-1")) hover_zoom = hover_zoom_1;
+		if (galery.hasClass("simplery-2")) hover_zoom = hover_zoom_2;
+		if (galery.hasClass("simplery-3")) hover_zoom = hover_zoom_3;
+		if (galery.hasClass("simplery-4")) hover_zoom = hover_zoom_4;
+		if (galery.hasClass("simplery-5")) hover_zoom = hover_zoom_5;
+
+		img.simpleryBoxResizeImage(blockWidth + hover_zoom);
+
+		var spaceToLeft = li.offset().left + parseFloat(img.css("left"));
+		var spaceToRight = $(window).width()
+			- li.offset().left 
+			- blockWidth
+			- (((blockWidth + hover_zoom) * ratio) - blockWidth) / 2;
+
+		if (spaceToLeft < hover_offset_border && spaceToRight > hover_offset_border) {
+			img.css("left", parseFloat(img.css("left")) - spaceToLeft + hover_offset_border);
+		}
+		if (spaceToRight < hover_offset_border && spaceToLeft > hover_offset_border) {
+			img.css("left", parseFloat(img.css("left")) + spaceToRight - hover_offset_border);
+		}
+		if (spaceToRight < hover_offset_border && spaceToLeft < hover_offset_border) {
+			img.simpleryBoxResizeImage(blockWidth/1.5);
+		}
+	}
+	else {
+		img.simpleryBoxResizeImage(blockWidth);
+	}
+
+	return this;
+};})( jQuery );
+
+
+/*
+ * resize image
+*/
+(function( $ ){$.fn.simpleryBoxResizeImage = function(width) {
+	var img = $(this);
+	var li = img.parent().parent();
+
+	var blockWidth = li.width();
+	var height = width;
+	var ratio = Math.round(img.width() / img.height() * 10) / 10;
+
+	// portrait
+	if (ratio < 1) {
+		height = width * 1 / ratio;
+	}
+	// landscape
+	else {
+		width = width * ratio;
+	}
+	
+	img.width(width);
+	img.height(height);
+
+	img.css("left", (blockWidth - width) / 2);
+	img.css("top", (blockWidth - height) / 2);
+
+	return this;
+};})( jQuery );
+
 
 
 
